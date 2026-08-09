53be6593934c652919f2ede80f09ed130ac60241
maglovskiNenad <maglovskin@gmail.com>
2026-08-09 23:59:35 +0200

    cpu

--- cmd/serverops/main.go
+++ cmd/serverops/main.go
@@ -4,10 +4,22 @@ import (
 	"fmt"
 	"net/http" //Go standard library package for HTTP servers and clients.
 
+	"github.com/maglovskiNenad/pingo/internal/metrics"
 	"github.com/maglovskiNenad/pingo/internal/router"
 )
 
 func main() {
+
+
+
+	cpu, err := metrics.CPUUsage()
+
+	if err != nil {
+		fmt.Println("CPU error:", err)
+	} else {
+		fmt.Printf("CPU Usage: %.1f%%\n", cpu)
+	}
+
 	// RegisterRoutes defines all HTTP routes used by the Pingo web application.
 	router.RegisterRoutes()
 
@@ -16,8 +28,9 @@ func main() {
 
 	// Start the HTTP server and listen on port 8080.
 	// nil means Go uses the default HTTP router (DefaultServeMux).
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 
+	
 	// Print an error if the server fails to start or stops unexpectedly.
 	if err != nil{
 		fmt.Println("Server error:", err)
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,14 +1,51 @@
 package router
 
-import "net/http"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/maglovskiNenad/pingo/internal/metrics"
+)
+
+type DashboardData struct {
+	CPU float64
+}
 
 // RegisterRoutes defines all HTTP routes used by the Pingo web application.
 func RegisterRoutes() {
 
-	// Serve the main dashboard page.
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "web/templates/index.html")
+		// Read the current CPU usage from the Linux system.
+		cpu, err := metrics.CPUUsage()
+		if err != nil {
+			http.Error(w, "Failed to read CPU usage", http.StatusInternalServerError)
+			return
+		}
+	
+		// Load the dashboard HTML template.
+		tmpl, err := template.ParseFiles("web/templates/index.html")
+			if err != nil {
+				http.Error(w, "Failed to load template", http.StatusInternalServerError)
+				return
+		}
+	
+		// Prepare the data that will be available inside the HTML template.
+		data := DashboardData{
+			CPU: cpu,
+		}
+	
+		// Render the HTML template and send it to the browser.
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			fmt.Println("Template error:", err)
+		}
 	})
+	
+	// Serve the main dashboard page.
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	http.ServeFile(w, r, "web/templates/index.html")
+	//})
 
 	// Serve the system information page.
 	http.HandleFunc("/system", func(w http.ResponseWriter, r *http.Request) {
