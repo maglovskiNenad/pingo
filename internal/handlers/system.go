a86ef08b0ba2e75236d1714e6b4ef11f437d6237
maglovskiNenad <maglovskin@gmail.com>
2026-08-12 23:18:26 +0200

    cpu

---
+++ internal/handlers/system.go
@@ -0,0 +1,63 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/maglovskiNenad/pingo/internal/metrics"
+)
+
+type SystemData struct {
+	CPU float64
+}
+
+func SystemHandler(w http.ResponseWriter, r *http.Request) {
+	// Read the current CPU usage from the Linux system.
+	cpu, err := metrics.CPUUsage()
+	if err != nil {
+		http.Error(w, "Failed to read CPU usage", http.StatusInternalServerError)
+		return
+	}
+
+	// Load the system HTML template.
+	tmpl, err := template.ParseFiles("web/templates/system.html")
+	if err != nil {
+		http.Error(w, "Failed to load system template", http.StatusInternalServerError)
+		return
+	}
+	
+
+	// Prepare the data for the HTML template.
+	data := SystemData{
+		CPU: cpu,
+	}
+
+	// Render the template and send it to the browser.
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		fmt.Println("Template error:", err)
+	}
+}
+
+func CPUAPIHandler(w http.ResponseWriter, r *http.Request) {
+	//Read the current CPU usafe
+	cpu, err := metrics.CPUUsage()
+	if err != nil {
+		http.Error(w, "Failed to read CPU usage", http.StatusInternalServerError)
+		return
+	}
+
+	// Tell the browser that the response contains JSON.
+	w.Header().Set("Content-Type", "application/json")
+
+	//Tell the browser that the respones contains JSON
+	err = json.NewEncoder(w).Encode(map[string]float64{
+		"cpu": cpu,
+	})
+
+	if err != nil {
+		fmt.Println("JSON error:", err)
+	}
+}
